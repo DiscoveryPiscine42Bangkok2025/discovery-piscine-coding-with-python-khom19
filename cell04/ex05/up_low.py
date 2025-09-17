843ee2fe67e3de579f1efc19162122b6e43ad80f
khom19 <khom2548@gmail.com>
2025-09-17 22:28:03 +0700

    add 4 01,02,03,04,05

---
+++ cell04/ex05/up_low.py
@@ -0,0 +1,10 @@
+words = list(input())
+
+for i in range(len(words)):
+    if words[i].islower():
+        words[i] = words[i].upper()
+    else:
+        words[i] = words[i].lower()
+        
+words = "".join(words)
+print(words)
